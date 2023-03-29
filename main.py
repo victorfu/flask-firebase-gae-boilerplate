ea9a6aa4b9b90fb75b55bcb8278bd2bb1e18191c
Victor Fu <supergothere@gmail.com>
2023-03-29 16:51:51 +0800

    feat: connect to firebase via admin sdk

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 # [START app]
 import logging
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
 from flask import Flask
 
-
 app = Flask(__name__)
+
+cred = credentials.Certificate("firebase-admin.json")
+firebaseApp = firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello!"
+
+
+@app.route("/api/<collection>")
+def get_collection(collection):
+    docs = db.collection(collection).stream()
+    output = []
+    for doc in docs:
+        dictDoc = doc.to_dict()
+        dictDoc["id"] = doc.id
+        output.append(dictDoc)
+    return output
 
 
 @app.errorhandler(500)
 def server_error(e):
-    # Log the error and stacktrace.
     logging.exception("An error occurred during a request.")
     return "An internal error occurred.", 500
 
